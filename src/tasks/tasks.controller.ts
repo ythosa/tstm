f8281a8ca2ce395e1cf221174c36fae45cc75616
ythosa <vasus714@yandex.ru>
2020-09-29 16:55:32 +0300

    Create Task DTO

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
+import { CreateTaskDTO } from './dto/create-task.dto';
+import { create } from 'domain';
 
 @Controller('tasks')
 export class TasksController {
@@ -12,10 +14,7 @@ export class TasksController {
     }
 
     @Post()
-    createTask(
-        @Body('title') title: string,
-        @Body('description') description: string,
-    ): Task {
-        return this.tasksService.createTask(title, description)
+    createTask(@Body() createTaskDTO: CreateTaskDTO): Task {
+        return this.tasksService.createTask(createTaskDTO)
     }
 }
