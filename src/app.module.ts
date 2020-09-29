64e42d83162ab15ac607131d5b27ba27ef5f7006
ythosa <vasus714@yandex.ru>
2020-09-29 23:09:57 +0300

    TypeORM config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-    imports: [TasksModule],
+    imports: [
+        TypeOrmModule.forRoot(typeOrmConfig),
+        TasksModule, 
+    ],
     providers: [],
 }) 
 export class AppModule {}
